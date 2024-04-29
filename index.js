f5b2bd0d2ee487f3a0a061af0c1800d8074bfcfd
soumyantspl <soumya.mishra@ntspl.co.in>
2024-04-29 16:14:39 +0530

    initial project set up

--- index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const app = express()
-const port = 3000
-console.log("In index.js file");
-app.listen(port, () => {
-    console.log(`MOM application is running on ${port}`);
-}) 
