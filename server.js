159a346d113be00a8c7dae5fc68682a05f64a40e
soumyantspl <soumya.mishra@ntspl.co.in>
2024-04-30 12:06:30 +0530

    common router added

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
 
+const express = require('express');
+const app = express();
+const mainRouter=require("./routers/index")
+const port=8000
+app.use('/api',mainRouter)
+app.listen(port, ()=>{
+    console.log("Server is running on port:- "+port);
+});
