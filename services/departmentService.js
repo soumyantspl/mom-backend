ec1d19643b17c110608d93570f40602a9771fcc3
HariharNayak <node.js@ntspl.co.in>
2024-05-08 15:32:07 +0530

    "Code modification in filtering API's of Department and Designations"

--- services/departmentService.js
+++ services/departmentService.js
@@ -29,10 +29,34 @@ const deleteDepartmentService = async (id) => {
   const deletedDepartment = await Department.findByIdAndDelete(id);
   return deletedDepartment;
 };
-const listDepartmentService = async () => {
-  const departments = await Department.find();
-  return departments;
+const listDepartmentService = async (bodyData, queryData) => {
+  const { order } = queryData;
+  const { organizationId, searchKey } = bodyData;
+  let query = searchKey
+    ? {
+        organizationId,
+        title: searchKey,
+        isActive: true,
+      }
+    : {
+        organizationId,
+        isActive: true,
+      };
+
+  var limit = parseInt(queryData.limit);
+  var skip = (parseInt(queryData.page) - 1) * parseInt(limit);
+  const totalCount = await Department.countDocuments(query);
+  const departmentList = await Department.find(query)
+    .sort({ createdAt: parseInt(order) })
+    .limit(limit)
+    .skip(skip);
+
+  return {
+    totalCount,
+    departmentList,
+  };
 };
+// };
 module.exports = {
   createDepartmentService,
   editDepartmentService,
