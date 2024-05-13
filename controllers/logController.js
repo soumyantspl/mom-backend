b97ecb955f71f1ca4d75d920d9a952c887075366
soumyantspl <soumya.mishra@ntspl.co.in>
2024-05-13 18:43:45 +0530

    view logs api completed

--- controllers/logController.js
+++ controllers/logController.js
@@ -4,25 +4,31 @@ const messages = require("../constants/constantMessages");
 
 /**FUNC- TO VIEW LOG LISTS**/
 const viewLogs = async (req, res) => {
-    try {
-      const result = await logService.viewLogs(req.body,req.query);
-      console.log(result);
-      if (result.totalCount==0) {
-        return Responses.failResponse(req, res, null, messages.recordsNotFound, 409);
-      }
-      return Responses.successResponse(
+  try {
+    const result = await logService.viewLogs(req.body, req.query);
+    console.log(result);
+    if (result.totalCount == 0) {
+      return Responses.failResponse(
         req,
         res,
-        result,
-        messages.recordsFound,
-        200
+        null,
+        messages.recordsNotFound,
+        409
       );
-    } catch (error) {
-      console.log(error);
-      return Responses.errorResponse(req, res, error);
     }
-  };
-
-  module.exports={
-    viewLogs
+    return Responses.successResponse(
+      req,
+      res,
+      result,
+      messages.recordsFound,
+      200
+    );
+  } catch (error) {
+    console.log(error);
+    return Responses.errorResponse(req, res, error);
   }
+};
+
+module.exports = {
+  viewLogs,
+};
--- services/logsService.js
+++ services/logsService.js
@@ -1,5 +1,7 @@
 const Logs = require("../models/logsModel");
+const ObjectId = require("mongoose").Types.ObjectId;
 
+/**FUNC- CREATE LOG */
 const createLog = async (data) => {
   const logData = await Logs.findOne({}).sort({ createdAt: -1 });
   data.serialNumber = logData ? logData.serialNumber + 1 : 1;
@@ -7,58 +9,67 @@ const createLog = async (data) => {
   return await newLogs.save();
 };
 
-const viewLogs = async  (bodyData, queryData) => {
-  
-    const { order } = queryData;
-    const { organizationId, searchKey } = bodyData;
-    let query = searchKey
-      ? {
-          organizationId,
-          details: searchKey
-        }
-      : {
-          organizationId
-        };
-  
-    const limit = parseInt(queryData.limit);
-    const skip = (parseInt(queryData.page) - 1) * parseInt(limit);
-    const totalCount = await Logs.countDocuments(query);
-    // const logsDatas = await Logs.find(query)
-    //   .sort({ createdAt: parseInt(order) })
-    //   .limit(limit)
-    //   .skip(skip);
-  console.log(query)
-      const logsDatas = await Logs.aggregate([
-        {
-          $match:query
+/**FUNC- LIST ALL LOG */
+const viewLogs = async (bodyData, queryData) => {
+  const { order } = queryData;
+  const { searchKey } = bodyData;
+  const organizationId = new ObjectId(bodyData.organizationId);
+  let query = searchKey
+    ? {
+        organizationId,
+        details: searchKey,
+      }
+    : {
+        organizationId,
+      };
+
+  const limit = parseInt(queryData.limit);
+  const skip = (parseInt(queryData.page) - 1) * parseInt(limit);
+  const totalCount = await Logs.countDocuments(query);
+
+  console.log("query", query);
+  const logsDatas = await Logs.aggregate([
+    {
+      $match: query,
+    },
+   
+    {
+      $lookup: {
+        from: "employees",
+        localField: "userId",
+        foreignField: "_id",
+        as: "userDetail",
+      },
+    },
+    {
+      $project: {
+        moduleName: 1,
+        serialNumber: 1,
+        action: 1,
+        ipAddress: 1,
+        details: 1,
+        createdAt: 1,
+        updatedAt: 1,
+        userDetail: {
+          name: 1,
+          _id: 1,
         },
-        {
-          $lookup: {
-            from: "employees",
-            localField: "userId",
-            foreignField: "_id",
-            as: "userDetail",
-          },
-        },
-        // {
-        //   $project: {
-        //     userDetail: {
-        //       name: 1,
-        //       _id: 1,
-        //     },
-        //   },
-        // },
-        { $unwind: "$userDetail" },
-      ])
-    //   .sort({ createdAt: parseInt(order) })
-    //     .limit(limit)
-    //     .skip(skip);
-    return {
-      totalCount,
-      logsDatas,
-    };
+      },
+    },
+    { $unwind: "$userDetail" }
+  ])
+  .sort({ createdAt: parseInt(order) })
+  .skip(skip)
+    
+      .limit(limit)
+      
+  return {
+    totalCount,
+    logsDatas,
   };
+};
 
 module.exports = {
-  createLog,viewLogs
+  createLog,
+  viewLogs,
 };
